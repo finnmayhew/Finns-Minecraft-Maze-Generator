4491759fc6666c70581ad5074f7605a2dba283b0
Mayhew, Finn <mayhewfi@msu.edu>
2023-06-08 00:30:17 -0400

    Tuned wall weighting

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ Maze config file
 '''
 
 # Weights for walls generating open or closed, [open, closed]
-wallWeights = [1, 2]
+wallWeights = [1, 15]
 
 # Weights for room types, [normal, chest]
 roomWeights = [10, 1]
